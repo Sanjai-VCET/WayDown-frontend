8a5378ab28bc75cec07be16dfaef3adde0e85390
Sanjaiweb <sanjaisanjai7182@gmail.com>
2025-04-24 10:11:45 +0530

    deployment changes cors

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://waydown-backend.onrender.com", // Adjust if your backend runs on a different port
   timeout: 10000,
+  withCredentials: true,
 });
 
 // Request interceptor to add token
